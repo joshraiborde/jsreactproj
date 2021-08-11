a8316db0b0eb527c6c764f2d7a8810f83dbefb85
joshraiborde <joshraiborde@gmail.com>
2021-08-11 13:15:34 -0400

    added Route exact paths to Home, About, Contact and Projects

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import Projects from './Projects'
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
   return <div className="App" >
-  <Home />
-  <About />
-  <Contact />
-  <Projects />
+  <Route exact path="/" component={Home} />
+  <Route exact path="/about" component={About} />
+  <Route exact path="/contact" component={Contact} />
+  <Route exact path="/projects" component={Projects} />
   </div>;
 }
 
