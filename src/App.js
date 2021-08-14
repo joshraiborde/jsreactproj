d22882636fc4d9b3e61253c05cc95031209fb241
joshraiborde <joshraiborde@gmail.com>
2021-08-14 17:23:46 -0400

    changed name to installed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 // import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Projects from './Projects';
+import Installed from './Installed';
+import Redundant from './Redundant';
+import Slowest from './Slowest';
 import { Route } from 'react-router-dom';
 import NavBar from './NavBar';
 
@@ -12,9 +12,9 @@ function App() {
   return <div className="App" >
   <NavBar />
   <Route exact path="/" component={Home} />
-  <Route exact path="/about" component={About} />
-  <Route exact path="/contact" component={Contact} />
-  <Route exact path="/projects" component={Projects} />
+  <Route exact path="/installed" component={Installed} />
+  <Route exact path="/redundant" component={Redundant} />
+  <Route exact path="/slowest" component={Slowest} />
   </div>;
 }
 
