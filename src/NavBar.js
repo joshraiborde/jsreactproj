02b1ac1c0ec0f4218a3608a7b8a91e59aec75d9f
joshraiborde <joshraiborde@gmail.com>
2021-08-14 17:30:56 -0400

    changed the names from home, about, contact and projects to HOME, INSTALLED, REDUNDANT, SLOWEST

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,9 +5,9 @@ function NavBar(){
     return (
         <ul>
             <li> <Link to='/'>Home</Link></li>
-            <li> <Link to='/about'>About</Link></li>
-            <li> <Link to='/contact'>Contact</Link></li>
-            <li> <Link to='/projects'>Projects</Link></li>
+            <li> <Link to='/installed'>Installed</Link></li>
+            <li> <Link to='/redundant'>Redundant</Link></li>
+            <li> <Link to='/slowest'>Slowest</Link></li>
         </ul>
     );
 }
