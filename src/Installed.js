bd55a215b6f9840cf750c113c6f3c9e42eb060ec
joshraiborde <joshraiborde@gmail.com>
2021-08-14 17:28:03 -0400

    changed name to installed from about

---
+++ src/Installed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Installed() {
+    return (
+        <div>
+            <h1>My Installed Extensions</h1>
+        </div>
+    )
+}
+
+export default Installed;
