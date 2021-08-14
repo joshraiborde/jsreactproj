a4a7f6d5507e7c2619c51e13df9145c74b64b2b6
joshraiborde <joshraiborde@gmail.com>
2021-08-14 17:28:51 -0400

    changed the name to redundant from contact

---
+++ src/Redundant.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Redundant() {
+    return (
+        <div>
+            <h1>My Redundant Extensions</h1>
+        </div>
+    )
+}
+
+export default Redundant;
