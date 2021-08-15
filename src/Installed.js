83551809e2f7af21a0e06a54ea900337775aee4e
joshraiborde <joshraiborde@gmail.com>
2021-08-15 15:45:05 -0400

    added all the extension data

--- src/Installed.js
+++ src/Installed.js
@@ -5,6 +5,318 @@ function Installed() {
     return (
         <div>
             <h1>My Installed Extensions</h1>
+            <div className="card" >
+            <h2>Auto Close Tag</h2>
+                <p>Version: v0.5.12</p>
+                <p>Creator: Jun Han</p>
+                <p>Downloads: 5,207,281</p>
+                <p>Description: Automatically add HTML/XML close tag, same as Visual Studio IDE or Sublime Text
+    global: This extension is enabled globally.</p>
+    <hr/>
+    <h2>Babel JavaScript</h2>
+<p>Version: v0.0.31</p>
+<p>Creator: Michael McDermott</p>
+<p>Downloads: 1,152,928</p>
+<p>Description: VSCode syntax highlighting for today's JavaScript</p>
+<p>Global: This extension is enabled globally. </p>
+<hr/>
+<h2>Better Comments</h2>
+<p>Version: v2.1.0</p>
+<p>Creator: Aaron Bond</p>
+<p>Downloads: 2,016,121</p>
+<p>Description: Improve your code commenting by annotating with alert, informational, TODOs, and more! </p>
+<p>Global: This extension is enabled globally. </p>
+<hr/>
+<h2>Blockman - Highlight Nested Code Blocks</h2>
+<p>Version: v1.2.0</p>
+<p>Creator: leodevbro</p>
+<p>Downloads: 18,955</p>
+<p>Description: Mark/Highlight code blocks</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Bracket Pair Colorizer</h2>
+<p>Version: v1.0.61</p>
+<p>Creator: CoenraadS</p>
+<p>Downloads: 5,908,409</p>
+<p>Description: A customizable extension for colorizing matching brackets</p>
+<p>Global: This extension is enabled globally.</p>
+
+<hr/>
+<h2>C#</h2>
+<p>Version: v1.23.14</p>
+<p>Creator: Microsoft</p>
+<p>Downloads: 12,230,709</p>
+<p>Description: C# for Visual Studio Code (powered by OmniSharp).
+Set Color Theme</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Custom Window Zoom</h2>
+<p>Version: v1.1.4</p>
+<p>Creator: craigb85</p>
+<p>Downloads: 4,380</p>
+<p>Description: This extension allows you to change the zooom level percentage that VSCode uses when zooming in & out. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>DotENV</h2>
+<p>Version: v1.0.1</p>
+<p>Creator: mikestead</p>
+<p>Downloads: 1,923,351</p>
+<p>Description: Support for dotenv file syntax</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Elm Emmet</h2>
+v1.0.1
+<p>Creator: Max Vyz. </p>
+<p>Downloads: 189,376</p>
+<p>Description: Convert emmet abbreviations to composition of elm functions</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>endwise</h2>
+<p>Version: v1.5.1</p>
+<p>Creator: Kai Wood</p>
+<p>Downloads: 130,031</p>
+<p>Description: Wisely add "end" in Ruby. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>erb</h2>
+<p>Version: v0.0.1</p>
+<p>Creator: Craig Maslowski</p>
+<p>Downloads: 105,432</p>
+<p>Description: Syntax Highlighting for Ruby ERB files</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>ES7 React/Redux/GraphQL/React-Native snippets</h2>
+<p>Version: v3.1.1</p>
+<p>Creator: dsznajder</p>
+<p>Downloads: 3,286,168</p>
+<p>Description: Simple extensions for React, Redux and Graphql in JS/TS with ES7 syntax</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>ESLint</h2>
+<p>Version: v2.1.23</p>
+<p>Creator: Dirk Baeumer</p>
+<p>Downloads: 15,817,860</p>
+<p>Description: Integrates ESLint JavaScript into VS Code. </p>
+<p>Global: This extension is enabled globally.
+This extension is recommended based on the files you recently opened. </p>
+
+<hr/>
+<h2>Flatiron Survival Kit</h2>
+<p>Version: v0.0.7</p>
+<p>Creator: dwieb</p>
+<p>Downloads: 165</p>
+<p>Description: Just a few items to help get you through Flatiron♥️️</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>GitLens — Git supercharged</h2>
+<p>Version: v11.6.0</p>
+<p>Creator: Eric Amodio</p>
+<p>Downloads: 10,247,835</p>
+<p>Description: Supercharge the Git capabilities built into Visual Studio Code — Visualize code authorship at a glance via Git blame annotations and code lens, seamlessly navigate and explore Git repositories, gain valuable insights via powerful comparison commands, and so much more</p>
+<p>Global: This extension is enabled globally. </p>
+
+
+<h2>Guides</h2>
+<p>Version: v0.9.3</p>
+<p>Creator: spywhere</p>
+<p>Downloads: 475,212</p>
+<p>Description: An extension for more guide lines</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Highlight Matching Tag</h2>
+<p>Version: v0.10.1</p>
+<p>Creator: vincaslt</p>
+<p>Downloads: 879,598</p>
+<p>Description: Highlights matching closing and opening tags</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>indent-rainbow</h2>
+<p>Version: v7.5.0</p>
+<p>Creator: oderwat</p>
+<p>Downloads: 2,239,176</p>
+<p>Description: Makes indentation easier to read</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Indenticator</h2>
+<p>Version: v0.7.0</p>
+<p>Creator: SirTori</p>
+<p>Downloads: 276,553</p>
+<p>Description: Highlights your current indent depth</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>JS JSX Snippets</h2>
+<p>Version: v10.2.0</p>
+<p>Creator: sky ran</p>
+<p>Downloads: 287,303</p>
+<p>Description: Simple extensions for React, Redux in JS with babel and ES6 syntax</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Live Server</h2>
+<p>Version: v5.6.1</p>
+<p>Creator: Ritwick Dey</p>
+<p>Downloads: 13,898,546</p>
+<p>Description: Launch a development local Server with live reload feature for static & dynamic pages</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Markdown Extended</h2>
+<p>Version: v1.0.19</p>
+<p>Creator: jebbs</p>
+<p>Downloads: 35,871</p>
+<p>Description: Extended syntaxes to built-in markdown & What you see is what you get exporter. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Markdown Preview Enhanced</h2>
+<p>Version: v0.5.22</p>
+<p>Creator: Yiyi Wang</p>
+<p>Downloads: 2,281,275</p>
+<p>Description: Markdown Preview Enhanced ported to vscode</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Material Icon Theme</h2>
+<p>Version: v4.9.0</p>
+<p>Creator: Philipp Kief</p>
+<p>Downloads: 8,619,035</p>
+<p>Description: Material Design Icons for Visual Studio Code
+Set File Icon Theme</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>NuGet Package Manager</h2>
+<p>Version: v1.1.6</p>
+<p>Creator: jmrog</p>
+<p>Downloads: 636,923</p>
+<p>Description: Add or remove .NET Core 1.1+ package references to/from your project's .csproj or .fsproj file using Code's Command Palette. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Path Intellisense</h2>
+<p>Version: v2.4.0</p>
+<p>Creator: Christian Kohler</p>
+<p>Downloads: 5,192,051</p>
+<p>Description: Visual Studio Code plugin that autocompletes filenames</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Prettier - Code formatter</h2>
+<p>Version: v8.1.0</p>
+<p>Creator: Prettier</p>
+<p>Downloads: 14,376,441</p>
+<p>Description: Code formatter using prettier</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Quokka.js</h2>
+<p>Version: v1.0.390</p>
+<p>Creator: Wallaby.js</p>
+<p>Downloads: 1,218,049</p>
+<p>Description: JavaScript and TypeScript playground in your editor. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Ruby</h2>
+<p>Version: v0.28.1</p>
+<p>Creator: Peng Lv</p>
+<p>Downloads: 2,074,297</p>
+<p>Description: Ruby language support and debugging for Visual Studio Code</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Settings Sync</h2>
+<p>Version: v3.4.3</p>
+<p>Creator: Shan Khan</p>
+<p>Downloads: 2,658,030</p>
+<p>Description: Synchronize Settings, Snippets, Themes, File Icons, Launch, Keybindings, Workspaces and Extensions Across Multiple Machines Using GitHub Gist. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>SQL Server (mssql)</h2>
+<p>Version: v1.10.1</p>
+<p>Creator: Microsoft</p>
+<p>Downloads: 4,316,164</p>
+<p>Description: Develop Microsoft SQL Server, Azure SQL Database and SQL Data Warehouse everywhere</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>VSCode Ruby</h2>
+<p>Version: v0.28.0</p>
+<p>Creator: Stafford Brunk</p>
+<p>Downloads: 1,058,090</p>
+<p>Description: Syntax highlighing, snippet, and language configuration support for Ruby</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>vscode-icons</h2>
+<p>Version: v11.6.0</p>
+<p>Creator: VSCode Icons Team</p>
+<p>Downloads: 8,543,667</p>
+<p>Description: Icons for Visual Studio Code
+Set File Icon Theme</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>YAML</h2>
+<p>Version: v0.22.0</p>
+<p>Creator: Red Hat</p>
+<p>Downloads: 6,235,068</p>
+<p>Description: YAML Language Support by Red Hat, with built-in Kubernetes syntax support</p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Zoom Bar</h2>
+<p>Version: v1.4.0</p>
+<p>Creator: wraith13</p>
+<p>Downloads: 28,492</p>
+<p>Description: Zoom UI in status bar for vscode. </p>
+<p>Global: This extension is enabled globally. </p>
+
+<hr/>
+<h2>Bracket Pair Colorizer 2</h2>
+<p>Version: v0.2.1</p>
+<p>Creator: CoenraadS</p>
+<p>Downloads: 3,048,223</p>
+<p>Description: A customizable extension for colorizing matching brackets</p>
+<p>Global: This extension is d globally by the user. </p>
+
+
+<h2>Markdown All in One</h2>
+<p>Version: v3.4.0</p>
+<p>Creator: Yu Zhang</p>
+<p>Downloads: 2,874,750</p>
+<p>Description: All you need to write Markdown (keyboard shortcuts, table of contents, auto preview and more) </p>
+<p>Global: This extension is d globally by the user. </p>
+
+<hr/>
+<h2>Markdown Checkbox</h2>
+<p>Version: v1.7.1</p>
+<p>Creator: Philipp Kief</p>
+<p>Downloads: 34,094</p>
+<p>Description: With this extension you can create checkboxes in markdown and mark them quickly. </p>
+<p>Global: This extension is d globally by the user. </p>
+
+
+
+
+
+
+
+            </div>
         </div>
     )
 }
